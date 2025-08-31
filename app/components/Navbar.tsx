17169a2083fedf1d6c25f8a0256afb67bceb9553
Suyog Bhandari <suyog.bha.suyog1219@gmail.com>
2025-08-31 15:09:50 +0545

    next js

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,40 @@
-const Navbar = () => {
+import Link from "next/link"
+import { auth, signIn, signOut } from "@/auth";
+import Image from "next/image"
+
+const Navbar = async() => {
+    const session =await auth();
+
   return (
-    <div>Navbar</div>
+    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+        <nav className="flex justify-between items-center">
+            <Link href="/">
+                <Image src="/logo.png" alt="logo" width={144} height={30} />
+            </Link>
+
+            <div className="flex items-center gap-5 text-black">
+                   { session && session?.user ? (
+                        <>
+                            <Link href="/startup/create">
+                                <span>Create</span>
+                            </Link>
+
+                            <button onClick={signOut}>
+                                <span>Logout</span>
+                            </button>
+
+                            <Link href={`/user/${session?.id}`}>
+                                <span>{session?.user?.name}</span>
+                            </Link>
+                        </>
+                   ):(
+                    <button onClick={signIn(provider:'github')}>
+                        <span>Login</span>
+                    </button>
+                   )} 
+            </div>
+        </nav>
+    </header>
   )
 }
 
